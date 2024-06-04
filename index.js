ca34d8da25df3e6c004bff43089fd1f8737bba7e
Tadukalamanasa12 <tadukalamanasa12@gmail.com>
2024-06-04 10:50:38 +0000

    react congrats card

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div className="container">
+    <h1>Congratulations</h1>
+    <div className="kiranContainer">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
